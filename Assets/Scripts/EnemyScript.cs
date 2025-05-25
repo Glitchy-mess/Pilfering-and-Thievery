78cec3e69ce6e49de902dc65edab161463644655
r-janvrin <ryan.janvrin@outlook.com>
2025-05-25 14:31:05 -0400

    Bag detection
    Guards and cameras can now see bags and will show that over their head using question marks

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.Properties;
 using Unity.VisualScripting;
 using UnityEditor.UIElements;
 using UnityEngine;
@@ -12,42 +14,55 @@ public class EnemyScript : MonoBehaviour
     public Transform[] points; //array of all the locations the guard can walk to 
     private int currentPosition = 0; //position in array of points we are at
     //variables for handling enemy navmesh
-    public NavMeshAgent agent;
-    public Transform player;
-    public bool validPoint = false;
-    public float walkPointRange = 5f;
+
+
     public LayerMask groundMask;
-    public Vector3 walkPoint;
+
+    
 
     //variables for enemy health
     [SerializeField]
     private int maxHealth = 100;
+
+    [SerializeField] private float playerHeight = 0.35f;//vertical offset for detection
+    [SerializeField] private float guardHeight = 0.35f;
+
+    //variables for checking if guard detects
+    public Transform player;
     public int alertState = 1;
-
-    public float playerHeight = 0.35f;//vertical offset for detection
-    public float guardHeight = 0.35f;
-
     private Vector3 playerVector; //Vector from guard to player
-    private Vector3 guardPosition;
-
     private float playerLength; //length of this vector
+    private Vector3 guardPosition;
+    private Vector3 lastSeenPosition; //place where guard last saw player
+    private Vector3 nextPosition;
+    private float dotProduct;
     public float detectionAngle = 0.35f;//cos of the angle of detection - closer to 1 is smaller
-    private float directionLength;
+    public float detectionRadius = 25f;
+    private RaycastHit hit;
+    int framesSinceLast = 0;
+    private GameObject[] allOfType;
+    private float currentBagDetection = 0;
 
+    //variables for changing of guard detection
     public float currentDetection = 0;
-    // these 
+    private float timeSinceSawPlayer = 0;
     public float detectionGainSpeed = 1;
     public float detectionLossSpeed = 1;
-
     public float maxDetection = 10;
-    public float detectionRadius = 25f;
-    public float waitTime = 5f; //how long the guard waits at each point before going to the next one
+
+    //variables for guards navigation
+    public NavMeshAgent agent;
+    private float waitTime = 5f; //how long the guard waits at each point before going to the next one
     private float currentWait = 0;
 
-    private RaycastHit hit;
+
+
+    //variables for combat / death
     public int currentHealth;
-    [SerializeField]
-    private bool bodyBaggable = false;
+    [SerializeField] private bool bodyBaggable = false;
+    [SerializeField] private LayerMask interactionLayer;
+
+    DetectionDisplayScript DetectionDisplay;
 
     // Start is called before the first frame update
     void Start()
@@ -57,6 +72,7 @@ public class EnemyScript : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         currentHealth = maxHealth;
         goToNextPoint();
+        DetectionDisplay = GetComponentInChildren<DetectionDisplayScript>();
     }
 
     // Update is called once per frame
@@ -71,21 +87,6 @@ public class EnemyScript : MonoBehaviour
             //check what alert state the guard is in and decide what to do
             if (alertState == 1) //stealth - patrolling and looking for player
             {
-                
-    
-                if (checkPlayerVisible())//increase detection if looking at player
-                {
-                    currentDetection += detectionGainSpeed * Time.deltaTime;
-                }
-                else //decrease detection
-                {
-                    currentDetection -= detectionLossSpeed * Time.deltaTime;
-                    if (currentDetection < 0)
-                    {
-                        currentDetection = 0;
-                    }
-                }
-    
                 //walk to next point or stay at current point
                 if (atDestination())//if we are at the end
                 {
@@ -95,11 +96,83 @@ public class EnemyScript : MonoBehaviour
                         goToNextPoint();
                     }
                 }
-    
+
+                if (checkPlayerVisible())//increase detection if looking at player
+                {
+                    currentDetection += detectionGainSpeed * Time.deltaTime;
+                    //check if player has been detected
+                    if (currentDetection > maxDetection)
+                    {
+                        alertThisGuard();
+                    }
+                }
+                else //decrease detection
+                {
+                    currentDetection -= detectionLossSpeed * Time.deltaTime;
+                    if (currentDetection < 0)
+                    {
+                        currentDetection = 0;
+                    }
+                }
+
+                if (rateLimit()) //this is a little performance heavy so we're only going to do it every 5 frames
+                {
+                    if (checkAnyBagVisible())//increase/decrease bag detection
+                    {
+                        currentBagDetection += detectionGainSpeed * 5 * Time.deltaTime;//account for only 1 in 5 frames
+                        if(currentBagDetection > maxDetection)
+                        {
+                            alertThisGuard();
+                        }
+                    }
+                    else
+                    {
+                        currentBagDetection -= detectionLossSpeed * 5 * Time.deltaTime; //account for only 1 in 5 frames
+                        if(currentBagDetection < 0)
+                        {
+                            currentBagDetection = 0;
+                        }
+                    }
+                }
+                //put a question mark or exclamation mark or nothing
+                updateOverheadIndicator();
             }
             else if(alertState == 2) //player has been found but mission is not loud
             {
-    
+                currentWait += Time.deltaTime;
+                timeSinceSawPlayer += Time.deltaTime;
+                if (checkPlayerVisible())
+                {
+                    timeSinceSawPlayer = 0;
+                    lastSeenPosition = player.position;
+                    //TODO: rotate toward player
+                }
+
+                //wait for the first 5 seconds
+                if(currentWait > 5)
+                {
+                    //go to player and try to arrest them
+                    agent.destination = lastSeenPosition;
+                    //check if player is in range
+                    if( (player.position - transform.position).sqrMagnitude < 0.25f)
+                    {
+                        Debug.Log("Player arrested");
+                    }
+                }
+                if(currentWait > 10)
+                {
+                    //sound the alarm if hasn't seen the player recently
+                    if(timeSinceSawPlayer > 1) {
+                        {
+                            alertAllGuards();
+                        } 
+                    }
+                }
+                if(currentWait > 20)
+                {
+                    //sound the alarm no matter what
+                    alertAllGuards();
+                }
             }
             else if(alertState == 3)//player has gone loud - chase and shoot
             {
@@ -123,7 +196,7 @@ public class EnemyScript : MonoBehaviour
         {//check if player is in detection angle
          //use dot product of 2 vectors: player vector and direction vector
          //direction vector is always a unit vector and has magnitude of 1
-            float dotProduct = playerVector.x * this.transform.forward.x + playerVector.y * this.transform.forward.y + playerVector.z * this.transform.forward.z;
+            dotProduct = playerVector.x * this.transform.forward.x + playerVector.y * this.transform.forward.y + playerVector.z * this.transform.forward.z;
 
             //rearrange the formula so we don't have to divide
             if (playerLength * detectionAngle < dotProduct) //if the player is in angle of detection
@@ -134,7 +207,40 @@ public class EnemyScript : MonoBehaviour
                 }
             }
         }
-        return false; ;
+        return false;
+    }
+
+    bool checkAnyBagVisible()
+    {
+        //check anything with tag "bag"
+        allOfType = GameObject.FindGameObjectsWithTag("Bag");
+        guardPosition = transform.position;
+        guardPosition.y += guardHeight;
+        foreach (GameObject bag in allOfType)
+        {
+            //yes this is a bunch of if statements
+            playerVector = bag.transform.position - guardPosition; //get vector from guard to bag - reusing player Vector
+            playerLength = playerVector.magnitude;
+            if(playerLength < detectionRadius)//check if bag in radius
+            {
+                //get dot product
+                dotProduct = playerVector.x * transform.forward.x + playerVector.y * transform.forward.y + playerVector.z * transform.forward.z;
+                if(playerLength * detectionAngle < dotProduct)//check if bag could be seen view range
+                {
+                    Debug.Log("firing ray");
+                    if (Physics.Raycast(guardPosition, playerVector, out hit, playerLength))
+                    {//check if the guard can actually see it
+                        Debug.Log("something hit");
+                        if( GameObject.ReferenceEquals(bag.gameObject, hit.transform.gameObject))//make sure the guard saw the bag
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+        }
+        return false;
     }
 
     bool atDestination()
@@ -156,6 +262,7 @@ public class EnemyScript : MonoBehaviour
     {
         gameObject.tag = "DeadGuard";
         bodyBaggable = true;
+        transform.gameObject.layer = interactionLayer; //allow for body bagging
     }
 
     void goToNextPoint()
@@ -173,5 +280,58 @@ public class EnemyScript : MonoBehaviour
         currentPosition = (currentPosition + 1) % points.Length;
         //reset wait time
         currentWait = 0;
+    }
+
+    void alertAllGuards() //change alert state of each guard to 3 and turn off all cameras
+    {
+        allOfType = GameObject.FindGameObjectsWithTag("Guard");
+        foreach(GameObject thatGuard in allOfType)
+        {
+            thatGuard.GetComponent<EnemyScript>().alertState = 3;
+        }
+
+        //turn off all cameras
+        allOfType = GameObject.FindGameObjectsWithTag("Camera");
+        foreach (GameObject thatCamera in allOfType)
+        {
+            thatCamera.GetComponent<cameraScript>().enabled = false;
+        }
+    }
+
+    void alertThisGuard()
+    {
+        alertState = 2;
+        currentWait = 0;
+        lastSeenPosition = player.position;
+        detectionAngle = 0; //give guard 360 degree vision
+    }
+
+    public void updateOverheadIndicator() //switch the detection indicator to the right one
+    {
+        //check if should display nothing
+        if(alertState > 2 || (currentDetection == 0 && currentBagDetection == 0))
+        {
+            DetectionDisplay.DisplayNothing();
+            return;
+        }
+        //display a question mark
+        DetectionDisplay.DisplayQuestion();
+
+        //check if should display exclamation mark instead
+        if(alertState == 2)
+        {
+            DetectionDisplay.DisplayExclamation();
+        }
+    }
+
+    bool rateLimit() //returns true the 5th time ran; otherwise false
+    {
+        framesSinceLast++;
+        if(framesSinceLast == 5)
+        {
+            framesSinceLast = 0;
+            return true;
+        }
+        return false;
     }
 }
---
+++ Assets/Scripts/cameraScript.cs
@@ -0,0 +1,211 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class cameraScript : MonoBehaviour
+{
+    //variables for detection
+    public Transform player;
+    private Vector3 playerVector; //Vector from guard to player
+    private Vector3 cameraPosition;
+    public float detectionRadius = 30f; //how far can detect
+    public float maxDetection = 10; //detection variables
+    private float playerLength = 0;
+    private float dotProduct;
+    private RaycastHit hit;
+    public GameObject[] allOfType;
+    private float playerHeight = 0.25f; //vertical offset of player in 
+
+    //variables for changing detection
+    public float currentDetection = 0;
+    public float detectionLossSpeed = 1;
+    public float detectionAngle = 0.25f; //cos of angle of detection - closer to 1 is smaller
+    private float currentBagDetection = 0;
+
+    public float maxWait = 5f; //how long to wait before sounding alarm
+    private float currentWait = 0f;
+    public float cameraOffset = 5f;//how far to move the camera
+    private bool playerFound = false;
+
+    private DetectionDisplayScript DetectionDisplay;
+    int framesSinceLast = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DetectionDisplay = GetComponentInChildren<DetectionDisplayScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (checkPlayerVisible())//if can see player, increase detection
+        {
+            currentDetection += Time.deltaTime;
+            if(currentDetection > maxDetection)
+            {
+                playerFound = true;
+            }
+        }
+        else //if can't, decrease detection
+        {
+            currentDetection -= detectionLossSpeed * Time.deltaTime;
+            if(currentDetection < 0)
+            {
+                currentDetection = 0;
+            }
+        }
+
+        if (rateLimit()) //check only once in 5
+        {
+            if (checkAnyBagVisible()) //if can see bag, increase detection
+            {
+                currentBagDetection += Time.deltaTime * 5;
+                if(currentBagDetection > maxDetection)
+                {
+                    playerFound = true;
+                }
+            }
+            else
+            {
+                currentBagDetection -= Time.deltaTime * 5;
+                if(currentBagDetection < 0)
+                {
+                    currentBagDetection = 0;
+                }
+            }
+        }
+
+
+        updateOverheadIndicator();
+
+        if (playerFound) //if the player has been detected, wait a lil before going loud
+        {
+            currentWait += Time.deltaTime;
+            if(currentWait > maxWait)
+            {
+                alertAllGuards();
+            }
+        }
+    }
+    bool checkPlayerVisible() //return true if player is seen, false if not
+    {
+        //determine if player is in sight or not
+        //"move" the camera forward so that the player can hide behind the camera
+        cameraPosition = transform.forward;
+        cameraPosition.y = 0; //make is so only moves horizontally
+        cameraPosition = cameraPosition * cameraOffset;
+        cameraPosition = cameraPosition + transform.position;
+
+        playerVector = player.position - cameraPosition; //get vector from camera to player
+        playerVector.y += playerHeight;
+
+        playerLength = playerVector.magnitude;
+
+        //check if player is in detection radius
+        if (playerLength < detectionRadius)
+        {//check if player is in detection angle
+         //use dot product of 2 vectors: player vector and direction vector
+         //direction vector is always a unit vector and has magnitude of 1
+
+            dotProduct = playerVector.x * this.transform.forward.x + playerVector.y * this.transform.forward.y + playerVector.z * this.transform.forward.z;
+
+            //rearrange the formula so we don't have to divide
+            if (playerLength * detectionAngle < dotProduct) //if the player is in angle of detection
+            {//make sure player isn't behind a wall
+                if (Physics.Raycast(cameraPosition, playerVector, out hit, playerLength))//if the raycast hit anything
+                {
+                    Debug.DrawRay(cameraPosition, playerVector, Color.red);
+                    return GameObject.ReferenceEquals(player.gameObject, hit.transform.gameObject);//make sure it hit the player
+                }
+
+            }
+        }
+        return false;
+    }
+
+    bool checkAnyBagVisible()
+    {
+        //check anything with tag "bag"
+        allOfType = GameObject.FindGameObjectsWithTag("Bag");
+
+        cameraPosition = transform.forward;
+        cameraPosition.y = 0; //make is so only moves horizontally
+        cameraPosition = cameraPosition * cameraOffset;
+        cameraPosition = cameraPosition + transform.position;
+
+        foreach (GameObject bag in allOfType)
+        {
+            //yes this is a bunch of if statements
+            playerVector = bag.transform.position - cameraPosition; //get vector from guard to bag - reusing player Vector
+            playerLength = playerVector.magnitude;
+            if (playerLength < detectionRadius)//check if bag in radius
+            {
+                //get dot product
+                dotProduct = playerVector.x * transform.forward.x + playerVector.y * transform.forward.y + playerVector.z * transform.forward.z;
+                if (playerLength * detectionAngle < dotProduct)//check if bag could be seen view range
+                {
+                    if (Physics.Raycast(cameraPosition, playerVector, out hit, playerLength))
+                    {//check if the guard can actually see it
+                        if (GameObject.ReferenceEquals(bag.gameObject, hit.transform.gameObject))//make sure the guard saw the bag
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+        }
+        return false;
+    }
+    void alertAllGuards()
+    {
+        //alert every guard
+        allOfType = GameObject.FindGameObjectsWithTag("Guard");
+        foreach (GameObject thatGuard in allOfType)
+        {
+            thatGuard.GetComponent<EnemyScript>().currentDetection = 0;
+            thatGuard.GetComponent<EnemyScript>().alertState = 3;
+            thatGuard.GetComponent<EnemyScript>().updateOverheadIndicator();
+        }
+
+        //turn off all cameras
+        allOfType = GameObject.FindGameObjectsWithTag("Camera");
+        foreach(GameObject thatCamera in allOfType)
+        {
+            thatCamera.GetComponent<cameraScript>().currentDetection = 0;
+            thatCamera.GetComponent<cameraScript>().updateOverheadIndicator();
+            thatCamera.GetComponent<cameraScript>().enabled = false;
+        }
+    }
+
+    public void updateOverheadIndicator()
+    {
+        //check if should display nothing
+        if (currentDetection == 0 && currentBagDetection == 0)
+        {
+            DetectionDisplay.DisplayNothing();
+            return;
+        }
+        DetectionDisplay.DisplayQuestion();
+        if(playerFound)
+        {
+            DetectionDisplay.DisplayExclamation();
+        }
+    }
+
+
+
+    bool rateLimit() //returns true the 5th time ran; otherwise false
+    {
+        framesSinceLast++;
+        if (framesSinceLast == 5)
+        {
+            framesSinceLast = 0;
+            return true;
+        }
+        return false;
+    }
+}
